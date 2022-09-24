58293143cbc7446bdc771d1782cc4389c25039cb
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-09-24 15:17:48 -0300

    Basic form structure and field validation with formik and yup
    Missing to create the repeating components

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Invalid email")
+        .required("This field is required"),
+      password: Yup.string().required("This field is required"),
+    }),
+  });
 
-export default Login
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="email@email.com"
+            name="email"
+            onChange={formik.handleChange}
+          />
+        </div>
+        {formik.errors.email && formik.touched.email && (
+          <h1>{formik.errors.email}</h1>
+        )}
+        <div>
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            className="form-control"
+            type="password"
+            placeholder="********"
+            name="password"
+            onChange={formik.handleChange}
+          />
+        </div>
+        {formik.errors.email && formik.touched.email && (
+          <h1>{formik.errors.password}</h1>
+        )}
+        <input type="submit" value="Login" className="btn btn-primary" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
