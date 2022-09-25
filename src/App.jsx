04dc45152cfc440279fa30146107bd5a4134359c
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-09-25 14:26:21 -0300

    Home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import {Routes, Route} from 'react-router-dom'
-import Login from './views/Login';
+import Login from './views/Login/Login';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Login/>}/>
+      <Route path='/home' element={<Login/>}/>
     </Routes>
   );
 }
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Login.scss"
 
-import FormLogin from "../components/forms/FormLogin";
-import BigLogo from "../components/logos/BigLogo";
+import FormLogin from "../../components/forms/FormLogin";
+import BigLogo from "../../components/logos/BigLogo";
 
 const Login = () => {
   return (
