c5f0d4185bac1252354828ad0a92d4f95f086dba
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-09-25 15:16:13 -0300

    Forget to change this xd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import {Routes, Route} from 'react-router-dom'
 import Login from './views/Login/Login';
+import Home from './views/Home/Home';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Login/>}/>
-      <Route path='/home' element={<Login/>}/>
+      <Route path='/home' element={<Home/>}/>
     </Routes>
   );
 }
